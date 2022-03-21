01b04eab658ca5d6e3595f682daeb73115edd61c
pablolucianop <pablolucianop@gmail.com>
2022-03-21 18:36:08 -0300

    moviendo state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Col from './Col';
+import Col2 from './Col2';
 import apiRequests from './apiRequests';
 import Banner from './Banner';
 import NavBar from './NavBar';
@@ -11,12 +12,15 @@ import './App.css';
 
 
 function App() {
+
   return (
     <div className="app">
       <NavBar/>
       <Sidebar />
       <Banner fetchUrl={apiRequests.fetchFeatMovie} />
       <Col title='populares' fetchUrl={apiRequests.fetchPopMovies} />
+      {console.log('info', info)}
+      <Col2 title='populares' movies={info} />
       {/* <Col title='mis películas' fetchUrl={indo} /> */}
       {/* <UploadFile /> */}
     </div>
