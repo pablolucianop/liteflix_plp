e8b395bd662303b3fac4fec2ee3b051540d80c38
pablolucianop <pablolucianop@gmail.com>
2022-03-24 17:27:46 -0300

    sidebar exp

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,48 @@
 import React from 'react'
 import './Sidebar.css'
-import { sidebarData } from './sidebarData'
 import plusHvyTransImg from './img/plusHvyTrans.svg'
 import close from './img/close.svg'
+
+const sidebarData = [
+  {
+    title: 'inicio',
+    link: 'home',
+  },
+  {
+    title: 'series',
+    link: 'home',
+  },
+  {
+    title: 'películas',
+    link: 'home',
+  },
+  {
+    title: 'agregadas recientemente',
+    link: 'home',
+  },
+  {
+    title: 'populares',
+    link: 'home',
+  },
+  {
+    title: 'mis películas',
+    link: 'home',
+  },
+  {
+    title: 'mi lista',
+    link: 'home',
+  },
+  {
+    title: 'agregar película',
+    link: 'upload',
+    icon: 'plusHvyTransImg',
+  },
+  {
+    title: 'cerrar sesión',
+    link: 'home',
+  },
+]
+
 function Sidebar(props) {
   return (
     <div className="sidebar">
